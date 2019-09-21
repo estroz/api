5d6a069fbde13212891b130cbfa30a43539997d7
Eric Stroczynski <ericstroczynski@gmail.com>
2019-09-20 17:24:01 -0700

    update crd validator

--- pkg/validate/crd.go
+++ pkg/validate/crd.go
@@ -5,84 +5,98 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/operator-framework/api/pkg/validate/validator"
 	"github.com/ghodss/yaml"
+	"github.com/operator-framework/api/pkg/validate/validator"
+	"github.com/operator-framework/operator-registry/pkg/appregistry"
 	"k8s.io/apiextensions-apiserver/pkg/apis/apiextensions"
 	"k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
 	"k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/validation"
 	"k8s.io/apimachinery/pkg/conversion"
-	"k8s.io/apimachinery/pkg/runtime"
 )
 
-type CRDValidator struct {
-	fileName string
-	crds     []v1beta1.CustomResourceDefinition
+type crdValidator struct {
+	fileNames []string
+	crds      map[appregistry.CRDKey]*v1beta1.CustomResourceDefinition
 }
 
-var _ validator.Validator = &CRDValidator{}
+var _ validator.Validator = &crdValidator{}
 
-func (v *CRDValidator) Validate() (results []validator.ManifestResult) {
-	for _, crd := range v.crds {
-		scheme := runtime.NewScheme()
-		result := crdInspect(crd, scheme)
-		if result.Name == "" {
-			result.Name = crd.GetName()
-		}
+func (v *crdValidator) Validate() (results []validator.ManifestResult) {
+	for key, crd := range v.crds {
+		result := validateCRD(crd)
+		result.Name = key.String()
 		results = append(results, result)
 	}
 	return results
 }
 
-func (v *CRDValidator) AddObjects(objs ...interface{}) validator.Error {
+func (v *crdValidator) AddObjects(objs ...interface{}) validator.Error {
 	for _, o := range objs {
 		switch t := o.(type) {
 		case v1beta1.CustomResourceDefinition:
-			v.crds = append(v.crds, t)
+			v.crds[apiKey(&t)] = &t
 		case *v1beta1.CustomResourceDefinition:
-			v.crds = append(v.crds, *t)
+			v.crds[apiKey(t)] = t
 		}
 	}
 	return validator.Error{}
 }
 
-func (v CRDValidator) Name() string {
+func apiKey(v *v1beta1.CustomResourceDefinition) appregistry.CRDKey {
+	// TODO: support multiple versions.
+	key := appregistry.CRDKey{Name: v.GetName(), Kind: v.Spec.Names.Kind}
+	key.Version = v.Spec.Version
+	if key.Version == "" {
+		if len(v.Spec.Versions) == 0 {
+			// QUESTION: is this unique enough, or should we just return an error
+			key.Version = "badVer" + key.String()
+		} else {
+			key.Version = v.Spec.Versions[0].Name
+		}
+	}
+	return key
+}
+
+func (v crdValidator) Name() string {
 	return "CustomResourceDefinition Validator"
 }
 
-func (v CRDValidator) FileName() string {
-	return v.fileName
+func (v crdValidator) FileName() string {
+	return ""
 }
 
-func (v CRDValidator) Unmarshal(rawYaml []byte) (interface{}, error) {
-	var crd v1beta1.CustomResourceDefinition
+func (v crdValidator) ObjectNames() []string {
+	return v.fileNames
+}
 
+// removeme
+func (v crdValidator) Unmarshal(rawYaml []byte) (interface{}, error) {
+	var crd v1beta1.CustomResourceDefinition
+	yaml.Marshal(crd)
 	rawJson, err := yaml.YAMLToJSON(rawYaml)
 	if err != nil {
-		return v1beta1.CustomResourceDefinition{}, fmt.Errorf("error parsing raw YAML to Json: %s", err)
+		return v1beta1.CustomResourceDefinition{}, fmt.Errorf("error decoding manifest: %v", err)
 	}
 	if err := json.Unmarshal(rawJson, &crd); err != nil {
-		return v1beta1.CustomResourceDefinition{}, fmt.Errorf("error parsing CRD (JSON) : %s", err)
+		return v1beta1.CustomResourceDefinition{}, fmt.Errorf("error parsing CRD (JSON): %v", err)
 	}
 	return crd, nil
 }
 
-func crdInspect(crd v1beta1.CustomResourceDefinition, scheme *runtime.Scheme) (manifestResult validator.ManifestResult) {
-	err := apiextensions.AddToScheme(scheme)
-	if err != nil {
-		return
-	}
-	err = v1beta1.AddToScheme(scheme)
-	if err != nil {
-		return
-	}
+func validateCRD(crd *v1beta1.CustomResourceDefinition) (manifestResult validator.ManifestResult) {
 	unversionedCRD := apiextensions.CustomResourceDefinition{}
-	scheme.Converter().Convert(&crd, &unversionedCRD, conversion.SourceToDest, nil)
+	err := Scheme.Converter().Convert(&crd, &unversionedCRD, conversion.SourceToDest, nil)
+	if err != nil {
+		verr := validator.Error{Type: validator.ErrorInvalidParse, Message: err.Error()}
+		manifestResult.Errors = append(manifestResult.Errors, verr)
+		return manifestResult
+	}
 	errList := validation.ValidateCustomResourceDefinition(&unversionedCRD)
 	for _, err := range errList {
 		if !strings.Contains(err.Field, "openAPIV3Schema") && !strings.Contains(err.Field, "status") {
-			er := validator.Error{Type: validator.ErrorType(err.Type), Field: err.Field, BadValue: err.BadValue, Detail: err.Error()}
-			manifestResult.Errors = append(manifestResult.Errors, er)
+			verr := validator.Error{Type: validator.ErrorType(err.Type), Field: err.Field, BadValue: err.BadValue, Message: err.Error()}
+			manifestResult.Errors = append(manifestResult.Errors, verr)
 		}
 	}
-	return
+	return manifestResult
 }
